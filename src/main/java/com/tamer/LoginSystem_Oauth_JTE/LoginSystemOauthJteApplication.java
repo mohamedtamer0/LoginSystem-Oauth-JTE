9ce4031e4fadc3f097392439ac175206498452a1
mohamedtamer0 <mt4452242@gmail.com>
2024-10-26 23:17:05 +0300

    add

---
+++ src/main/java/com/tamer/LoginSystem_Oauth_JTE/LoginSystemOauthJteApplication.java
@@ -0,0 +1,13 @@
+package com.tamer.LoginSystem_Oauth_JTE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSystemOauthJteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSystemOauthJteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamer/LoginSystem_Oauth_JTE/LoginSystemOauthJteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamer.LoginSystem_Oauth_JTE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSystemOauthJteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
